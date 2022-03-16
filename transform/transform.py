150e367b978286d5c616102a76fd2231ed19d82c
Emma <quinio.emma44@gmail.com>
2022-03-16 14:08:46 +0100

    working with csv now

--- transform/transform.py
+++ transform/transform.py
@@ -1,80 +1,85 @@
 import json
+import csv
   
-# chargement des données métiers
-with open("input/metiers.json", 'r', encoding='utf-8') as f:
-  metiers_data = json.load(f)
+metiers_dict = {}
 
-dict_metiers = {}
+# Lecture des données métiers
+with open('input/metiers.csv', 'r', encoding='utf-8') as csvfile:
+  data = csv.reader(csvfile, delimiter=',', quotechar='"')
+  next(data)
+  # Enregistrement des données métiers
+  for row in data:
+    metiers_dict[row[0]] = {
+      "libelle": row[1],
+      "domaine" : { "code" : row[2], "libelle" : row[3] },
+      "domaine_pro" : { "code" : row[4], "libelle" : row[5] },
+      "competences" : [],
+      "exemples" : []
+      }
 
-# Transformation des données métiers
-for metier in metiers_data["metiers"]:
-  code_rom = metier["code_rom"]
-  if code_rom in dict_metiers:
-    dict_metiers[code_rom]["metiers"] += [metier["label"]]
-  else:
-    dict_metiers[code_rom] = { "metiers": [metier["label"]]}
+# Lecture et enregistrement des données d'exemples métiers
+with open('input/metiers_exemple.csv', 'r', encoding='utf-8') as csvfile:
+  data = csv.reader(csvfile, delimiter=',', quotechar='"')
+  next(data)
+  for row in data: metiers_dict[row[3]]["exemples"] += [row[1]]
 
-# Chargement des données de correspondances entre métiers et compétences
-with open('input/correspondances.json', 'r', encoding='utf-8') as f:
-  correspondances_data = json.load(f)
-
-# Ajout des données de compétences
-for correspondance in correspondances_data["correspondances"]:
-  code_rom = correspondance["code_rom"]
-  if "competences" in dict_metiers[code_rom]:
-    dict_metiers[code_rom]["competences"] += [correspondance["code_ogr"]]
-  else:
-    dict_metiers[code_rom]["competences"] = [correspondance["code_ogr"]]
+# Lecture et enregistrement des données de correspondances entre métiers et compétences
+with open('input/correspondances.csv', 'r', encoding='utf-8') as csvfile:
+  data = csv.reader(csvfile, delimiter=',', quotechar='"')
+  next(data)
+  for row in data: metiers_dict[row[0]]["competences"] += [row[1]]
 
 # Chargement des données de mes compétences
 with open('input/mescompetences.json', 'r', encoding='utf-8') as f:
   mescompetences_data = json.load(f)
 
 # Création des données de mescompétences
-mescompetence = [mescompetence["code_ogr"] for mescompetence in mescompetences_data["mescompetences"]]
+mescompetence = [str(mescompetence["code_ogr"]) for mescompetence in mescompetences_data["mescompetences"]]
 
-# Chargement des données de compétences
-with open('input/competences.json', 'r', encoding='utf-8') as f:
-  competence_data = json.load(f)
-
-# Transformations des données compétences
 competences = {}
-for competence in competence_data["competences"]:
-  competences[competence["code_ogr"]] = competence["libelle"]
+
+# Lecture des données métiers
+with open('input/competences.csv', 'r', encoding='utf-8') as csvfile:
+  data = csv.reader(csvfile, delimiter=',', quotechar='"')
+  next(data)
+  # Enregistrement des données métiers
+  for row in data:
+    if not row[0] in competences:
+      competences[row[0]] = row[1]
 
 # Regarde pour chaque metier si tu es compatible
-for metier in dict_metiers:
-  n = len(dict_metiers[metier]["competences"])
+for metier in metiers_dict:
+  n = len(metiers_dict[metier]["competences"])
   score = 0
 
-  dict_metiers[metier]["competencesManquantes"] = []
+  metiers_dict[metier]["competencesManquantes"] = []
 
-  for competence in dict_metiers[metier]["competences"]:
+  for competence in metiers_dict[metier]["competences"]:
     if competence in mescompetence:
       score += 1
     else:
       if competence in competences:
-        dict_metiers[metier]["competencesManquantes"] += [competences[competence]]
+        metiers_dict[metier]["competencesManquantes"] += [competences[competence]]
       else:
-        dict_metiers[metier]["competencesManquantes"] += ["Compétence inconnue, code : " + str(competence)]
+        metiers_dict[metier]["competencesManquantes"] += ["Compétence inconnue, code : " + str(competence)]
 
-  dict_metiers[metier]["score"] = {
+  metiers_dict[metier]["score"] = {
     "resultat": score/n,
     "score": score,
     "surcombien": n
     }
 
 # Transformation de données et trie
-code_rom_trie = sorted(dict_metiers, key=lambda metier: (-dict_metiers[metier]["score"]["resultat"], -dict_metiers[metier]["score"]["score"]))
+code_rom_trie = sorted(metiers_dict, key=lambda metier: (-metiers_dict[metier]["score"]["score"], -metiers_dict[metier]["score"]["resultat"]))
 
 # Affiche les résultat
-for i in range(10):
-  resultat = dict_metiers[code_rom_trie[i]]
+for i in range(3):
+  resultat = metiers_dict[code_rom_trie[i]]
 
   print("Résultat top ", i + 1)
   print("Code métier : ", code_rom_trie[i])
   print("Qui corressponds aux métiers :")
-  for metier in resultat["metiers"]:
+  for metier in resultat["exemples"]:
     print(" - ", metier)
   print("Ton score est de : ", '{:.1%}'.format(resultat["score"]["resultat"]), " soit ", resultat["score"]["score"], "/", resultat["score"]["surcombien"])
   print("Pour avoir un meilleur score tu devrais travailler sur les compétences :")
@@ -82,4 +87,4 @@ for i in range(10):
     print(" - ", competence)
 
 # Jolie Affichage sous forme JSON
-# print(json.dumps(dict_metiers, indent = 4, sort_keys=True, ensure_ascii=False))
+# print(json.dumps(metiers_dict, indent = 4, sort_keys=True, ensure_ascii=False))
